34d92014fe6bc94b3918604cdac0fa791c180ddc
amadeulee <amadeulee@discente.ufg.br>
2024-04-01 21:00:40 -0300

    Initial commit

---
+++ src/main/java/com/ufg/sgr/SgrApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.sgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufg/sgr/SgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufg.sgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
